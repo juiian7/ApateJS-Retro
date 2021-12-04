df9cbe2a0a1ce6afb316c6aae86579410cd58f34
Julian <julian.kusternigg@gmail.com>
2021-12-04 10:37:49 +0100

    Implement extended sprite drawing

--- src/utils/drawlib.ts
+++ src/utils/drawlib.ts
@@ -22,6 +22,14 @@ export class DrawLib {
 
         if (chars.length != characters.length) console.error("Characters do not match with image!");
         for (let i = 0; i < characters.length; i++) {
+            // color white for color addition
+            for (let j = 0; j < chars[i].data.length; j += 4) {
+                if (chars[i].data[j + 3] > 0) {
+                    chars[i].data[j] = 255;
+                    chars[i].data[j + 1] = 255;
+                    chars[i].data[j + 2] = 255;
+                }
+            }
             this.fontMap[characters[i]] = chars[i];
         }
 
@@ -54,7 +62,36 @@ export class DrawLib {
             }
         }
     }
-    public text(x: number, y: number, text: string, c: Color) {
+    public spriteExt(x: number, y: number, sprite: Sprite, scale: number, color: Color) {
+        if (!sprite) {
+            console.warn("Sprite not ready! skipping draw");
+            return;
+        }
+        scale = Math.round(scale);
+
+        let i, px, py, dx, dy;
+        for (i = 0, px = 0, py = 0; i < sprite.data.length; i += 4, px += scale) {
+            if (px >= sprite.width * scale) {
+                px = 0;
+                py += scale;
+            }
+            if (sprite.data[i + 3] > 0) {
+                for (dx = 0; dx < scale; dx++) {
+                    for (dy = 0; dy < scale; dy++) {
+                        this.screen.setPixel(
+                            px + dx + x,
+                            py + dy + y,
+                            (sprite.data[i] + color.r) / 2,
+                            (sprite.data[i + 1] + color.g) / 2,
+                            (sprite.data[i + 2] + color.b) / 2
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    public text(x: number, y: number, text: string, c: Color, scale: number = 1) {
         text = text.toUpperCase();
 
         for (let i = 0, char; i < text.length; i++) {
@@ -62,14 +99,7 @@ export class DrawLib {
             char = this.fontMap[text[i]];
 
             if (char) {
-                for (let p = 0; p < char.data.length; p += 4) {
-                    if (char.data[p + 3] > 128) {
-                        char.data[p] = c.r;
-                        char.data[p + 1] = c.g;
-                        char.data[p + 2] = c.b;
-                    }
-                }
-                this.sprite(x + i * char.width, y, char);
+                this.spriteExt(x + i * char.width, y, char, scale, { r: (c.r - 128) * 2, g: (c.g - 128) * 2, b: (c.b - 128) * 2 }); // to overwrite color
             } else this.pixel(x + i * 4, y, c);
         }
     }
