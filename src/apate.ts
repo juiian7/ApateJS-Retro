c82bdf18cb0442288fc5b69a8740178bf5bec332
l1ino <94066019+l1ino@users.noreply.github.com>
2021-12-21 15:08:47 +0100

    Update imports in apate.ts

--- src/apate.ts
+++ src/apate.ts
@@ -1,12 +1,17 @@
+// ./core/ modules
 export { Engine as Apate } from "./core/Engine.js";
 export { Entity } from "./core/Entity.js";
+export { Input } from "./core/Input.js";
+export { ParticleSystem, Particle } from "./core/ParticleSystem.js";
 export { Scene } from "./core/Scene.js";
 export { Transition } from "./core/Transition.js";
-export { ParticleSystem, Particle } from "./core/ParticleSystem.js";
 
-export { Color } from "./utils/color.js";
+// ./utils/ modules
 export { Button } from "./utils/button.js";
+export { Color } from "./utils/color.js";
 export { Random } from "./utils/random.js";
 
-export { spritelib } from "./utils/spritelib.js";
 export { colorlib } from "./utils/colorlib.js";
+export { DrawLib } from "./utils/drawlib.js";
+export { PhysicLib } from "./utils/physiclib.js";
+export { spritelib } from "./utils/spritelib.js";
