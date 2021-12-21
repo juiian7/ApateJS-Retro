8e93f920903eb1ac3ed041477bc02c0094ea7b08
l1ino <94066019+l1ino@users.noreply.github.com>
2021-12-21 14:44:23 +0100

    Fix rollup issues

--- src/apate.ts
+++ src/apate.ts
@@ -4,9 +4,9 @@ export { Scene } from "./core/Scene.js";
 export { Transition } from "./core/Transition.js";
 export { ParticleSystem, Particle } from "./core/ParticleSystem.js";
 
-export { Color } from "./utils/Color.js";
-export { Button } from "./utils/Button.js";
-export { Random } from "./utils/Random.js";
+export { Color } from "./utils/color.js";
+export { Button } from "./utils/button.js";
+export { Random } from "./utils/random.js";
 
 export { spritelib } from "./utils/spritelib.js";
 export { colorlib } from "./utils/colorlib.js";
