d36ec33b840f8dd91a97a9c651f1c367376aaac6
l1ino <94066019+l1ino@users.noreply.github.com>
2022-02-23 20:12:59 +0100

    Refactor library distribution (#5)

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+// rollup.config.js
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+export default [
+    // apate main engine
+    {
+        input: "src/apate.ts",
+        output: {
+            dir: "dist",
+            format: "es",
+        },
+        plugins: [typescript({ tsconfig: "./tsconfig.json" })],
+    },
+    {
+        input: "dist/dts/apate.d.ts",
+        output: {
+            file: "dist/apate.d.ts",
+            format: "es",
+        },
+        plugins: [dts()],
+    },
+    // apate legacy-wrapper
+    {
+        input: "src/legacy-wrapper.ts",
+        output: {
+            dir: "dist",
+            format: "es",
+        },
+        plugins: [typescript({ tsconfig: "./tsconfig.json" })],
+    },
+    {
+        input: "dist/dts/legacy-wrapper.d.ts",
+        output: {
+            file: "dist/legacy-wrapper.d.ts",
+            format: "es",
+        },
+        plugins: [dts()],
+    },
+];
